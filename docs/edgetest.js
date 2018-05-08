f0a56e1b7d5d23588a4d7b1e87e618705a42e96c
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-05-08 10:29:30 +0900

    commit

--- docs/edgetest.js
+++ docs/edgetest.js
@@ -1,17 +1,13 @@
-let sig = null;
-let myId = null;
+let pcs = {};
 
 const peer = new Peer({
     key: 'bea1e09a-a7f9-41fb-8700-e6d18ba907bd',
-    iceTransportPolicy: 'relay',
-    debug: 3
 });
 peer.on('open', async id => {
-    myId = id;
     console.log('peer open.');
     myIdDisp.textContent = id;
-    sig = peer.socket._io;
-    console.log(`signalingURL: ${sig.io.uri}`);
+    console.log(`signalingURL: ${peer.socket._io.io.uri}`);
+    initSig(peer.socket._io);
 
     // peer.on('call', async call => {
     //     console.log('peer on call.');
@@ -32,10 +28,8 @@ peer.on('open', async id => {
     });
 });
 
-let pc = null;
-
 async function initPC(remoteId) {
-    pc = new RTCPeerConnection({
+    const pc = pcs[remoteId] = new RTCPeerConnection({
         iceServers: [
             { urls: 'stun:stun.l.google.com:19302' }
         ]
@@ -58,35 +52,61 @@ async function initPC(remoteId) {
     }
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        stream.getTracks().forEach(track => pc.addTrack(track, stream));
+        stream.getTracks().forEach(track => {
+            console.log('addTrack', `${track.kind}Track`);
+            pc.addTrack(track, stream);
+        });
     } catch (err) {
         console.log('gUM() error', err);
     }
+    return pc;
 }
 
-function sigEmit(type, data, remoteId) {
-    data = { ...data, ...{ src: myId, dst: remoteId } };
-
-}
-function setupCall(call) {
-    console.log('setupCall()');
-    call.on('stream', stream => {
-        console.log('call on stream');
-        remoteView.srcObject = stream;
+function initSig(sig) {
+    sig.on('offer', async data => {
+        console.log('sig on offer');
+        const pc = getOrCreatePC(data.src);
+        await pc.setRemoteDescription(data.offer);
+        const answer = await pc.createAnswer();
+        sigEmit('answer', {answer});
+    });
+    sig.on('answer', async data => {
+        await pcs[data.src].setRemoteDescription(data.answer);
+    });
+    sig.on('candidate', async data => {
+        const pc = getOrCreatePC(data.src);
+        await pc.addIceCandidate(data.candidate);
     });
 }
 
-async function getStream() {
-    console.log('getStream()');
-    let stream = null;
-    if (localView.srcObject) {
-        localView.srcObject.getTracks().forEach(track => track.stop());
-    }
-    if (navigator.userAgent.includes('Edge/17')) {
-        stream = await navigator.getDisplayMedia({ video: true });
-    } else {
-        stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    }
-    localView.srcObject = stream;
-    return stream;
+function getOrCreatePC(remoteId) {
+    return pcs[remoteId] || initPC();
 }
+
+function sigEmit(type, data, remoteId) {
+    data = { ...data, ...{ src: pcs[remoteId].id, dst: remoteId } };
+    peer.socket._io.emit(type, data);
+}
+
+// function setupCall(call) {
+//     console.log('setupCall()');
+//     call.on('stream', stream => {
+//         console.log('call on stream');
+//         remoteView.srcObject = stream;
+//     });
+// }
+
+// async function getStream() {
+//     console.log('getStream()');
+//     let stream = null;
+//     if (localView.srcObject) {
+//         localView.srcObject.getTracks().forEach(track => track.stop());
+//     }
+//     if (navigator.userAgent.includes('Edge/17')) {
+//         stream = await navigator.getDisplayMedia({ video: true });
+//     } else {
+//         stream = await navigator.mediaDevices.getUserMedia({ video: true });
+//     }
+//     localView.srcObject = stream;
+//     return stream;
+// }
